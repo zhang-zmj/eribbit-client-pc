1bbdbbbbc3915c467640cfe3af1cf1396534e7fa
zhangmj <15669993907@163.com>
2022-06-19 21:39:18 +0800

    23-首页主体-面板骨架效果

--- src/views/home/components/home-hot.vue
+++ src/views/home/components/home-hot.vue
@@ -1,14 +1,19 @@
 <template>
   <HomePanel title="人气推荐" sub-title="人气爆款 不容错过">
-    <ul ref="pannel" class="goods-list">
-      <li v-for="item in goods" :key="item.id">
-        <RouterLink to="/">
-          <img :src="item.picture" alt="" />
-          <p class="name">{{ item.title }}</p>
-          <p class="desc">{{ item.alt }}</p>
-        </RouterLink>
-      </li>
-    </ul>
+    <div style="position: relative; height: 426px">
+      <Transition name="fade">
+        <ul v-if="goods.length" ref="pannel" class="goods-list">
+          <li v-for="item in goods" :key="item.id">
+            <RouterLink to="/">
+              <img :src="item.picture" alt="" />
+              <p class="name">{{ item.title }}</p>
+              <p class="desc">{{ item.alt }}</p>
+            </RouterLink>
+          </li>
+        </ul>
+        <HomeSkeleton v-else />
+      </Transition>
+    </div>
   </HomePanel>
 </template>
 
--- src/views/home/components/home-new.vue
+++ src/views/home/components/home-new.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="home-new">
     <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
-      <template #right><XtxMore path="/" /></template>
-      <!-- 面板内容 -->
-      <ul class="goods-list">
-        <li v-for="item in goods" :key="item.id">
-          <RouterLink :to="`/product/${item.id}`">
-            <img :src="item.picture" alt="" />
-            <p class="name ellipsis">{{ item.name }}</p>
-            <p class="price">&yen;{{ item.price }}</p>
-          </RouterLink>
-        </li>
-      </ul>
+      <template #right><XtxMore /></template>
+      <div style="position: relative; height: 406px">
+        <Transition name="fade">
+          <!-- 面板内容 -->
+          <ul v-if="goods.length" ref="pannel" class="goods-list">
+            <li v-for="item in goods" :key="item.id">
+              <RouterLink :to="`/product/${item.id}`">
+                <img :src="item.picture" alt="" />
+                <p class="name ellipsis">{{ item.name }}</p>
+                <p class="price">&yen;{{ item.price }}</p>
+              </RouterLink>
+            </li>
+          </ul>
+          <HomeSkeleton bg="#f0f9f4" v-else />
+        </Transition>
+      </div>
     </HomePanel>
   </div>
 </template>
